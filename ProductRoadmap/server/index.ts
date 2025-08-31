a4ff002dd353bf99cc3d5f0832bf07984d59c687
Pranav Sawant <64312638+pranavsawant22@users.noreply.github.com>
2025-09-01 01:42:05 +0530

    Update index.ts

--- ProductRoadmap/server/index.ts
+++ ProductRoadmap/server/index.ts
@@ -61,6 +61,8 @@ app.use((req, res, next) => {
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
   const port = parseInt(process.env.PORT || "5000", 10);
-app.listen(port, "0.0.0.0", () => {
+  app.listen(port, "0.0.0.0", () => {
   log(`serving on port ${port}`);
 });
+}
+ 
